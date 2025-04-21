9317defbab22c3da305a6ea4069babe08fed2cc3
TaeGun08 <gustn4984@naver.com>
2025-04-21 17:19:39 +0900

    StagBeetle Enemy 추가

--- Assets/3.Scripts/Enemy/StagBeetle/StagBeetle.cs
+++ Assets/3.Scripts/Enemy/StagBeetle/StagBeetle.cs
@@ -35,9 +35,9 @@ public class StagBeetle : Enemy
         float distance = Vector3.Distance(new Vector3(transform.position.x, 0f, transform.position.z),
             new Vector3(localPlayer.transform.position.x, 0f, localPlayer.transform.position.z));
 
-        if (distance <= 5f)
+        if (distance <= 7f)
         {
-            agent.SetDestination(localPlayer.transform.position - transform.forward * 5f);
+            agent.SetDestination(localPlayer.transform.position - transform.forward * 6.5f);
             return;
         }
 
@@ -54,11 +54,47 @@ public class StagBeetle : Enemy
 
         attackDelay -= Time.deltaTime;
 
-        if (distance > 2f || target > chaseAngle * 0.5f || isDead || attackDelay > 0) return;
+        if (distance > 7f || target > chaseAngle * 0.5f || isDead || attackDelay > 0) return;
         animator.SetTrigger(ATTACK);
         int ran = Random.Range(0, hasPattern);
         animator.SetFloat(PATTERN, ran);
         isMoveStop = true;
         isPattern = true;
+        StartCoroutine(PatternCoroutine(ran));
+    }
+    
+    private IEnumerator PatternCoroutine(int pattern)
+    {
+        switch (pattern)
+        {
+            case 0:
+                yield return new WaitForSeconds(0.7f);
+                attackColliders[0].SetActive(true);
+                yield return new WaitForSeconds(1f);
+                break;
+            case 1:
+                yield return new WaitForSeconds(0.7f);
+                attackColliders[0].SetActive(true);
+                yield return new WaitForSeconds(1f);
+                break;
+            case 2:
+                yield return new WaitForSeconds(1.2f);
+                attackColliders[1].SetActive(true);
+                yield return new WaitForSeconds(1f);
+                break;
+            case 3:
+                yield return new WaitForSeconds(1.2f);
+                attackColliders[0].SetActive(true);
+                yield return new WaitForSeconds(1f);
+                break;
+        }
+        
+        attackColliders[0].SetActive(false);
+        attackColliders[1].SetActive(false);
+        
+        yield return new WaitForSeconds(1f);
+        isMoveStop = false;
+        isPattern = false;
+        attackDelay = Data.AttackDelay;
     }
 }
