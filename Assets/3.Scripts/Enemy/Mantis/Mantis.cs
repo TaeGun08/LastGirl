1a578b7be8cdd0aa055ad2bf7cb91896c5ed8efb
TaeGun08 <gustn4984@naver.com>
2025-04-21 18:58:19 +0900

    추가 중

--- Assets/3.Scripts/Enemy/Mantis/Mantis.cs
+++ Assets/3.Scripts/Enemy/Mantis/Mantis.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Mantis : Enemy
 {
+    private static readonly int IS_WALK = Animator.StringToHash("isWalk");
+
     [Header("MinerBeetle Settings")] 
     [SerializeField] private GameObject[] attackColliders;
     [SerializeField] private float chaseAngle;
     [SerializeField] private int hasPattern;
-
+    private bool isWalk;
+    
     private void OnDrawGizmos()
     {
         Vector3 leftBoundary = Quaternion.Euler(0, -chaseAngle / 2, 0) * transform.forward;
@@ -29,23 +32,25 @@ public class Mantis : Enemy
             Gizmos.DrawLine(transform.position, transform.position + rightBoundary * 5);
         }
     }
-    
+
     protected override void UpdateMovement()
     {
         float distance = Vector3.Distance(new Vector3(transform.position.x, 0f, transform.position.z),
             new Vector3(localPlayer.transform.position.x, 0f, localPlayer.transform.position.z));
 
-        if (distance <= 5f)
+        if (distance <= 7f)
         {
-            agent.SetDestination(transform.position);
+            agent.SetDestination(localPlayer.transform.position - transform.forward * 6.5f);
+            animator.SetBool(IS_WALK, false);
             return;
         }
 
         agent.SetDestination(localPlayer.transform.position);
+        animator.SetBool(IS_WALK, true);
     }
 
     protected override void UpdatePattern()
     {
- 
+        
     }
 }
